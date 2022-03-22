1f6acaa41a9fdbda21019c0db70a3ebc3ece4a0f
Adel <yassinadel@hotmail.com>
2022-03-22 21:47:53 +0200

    new empty activity created

---
+++ app/src/main/java/com/csc498/groupCurrencyConverter/ConversionActivity.java
@@ -0,0 +1,14 @@
+package com.csc498.groupCurrencyConverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConversionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversion);
+    }
+}
