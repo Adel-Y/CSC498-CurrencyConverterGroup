c4a4420603c5d6dc3d404c84c4039a3f36454887
husseinadkerning <hseneldowi@gmail.com>
2022-03-31 22:03:06 +0300

    fix ratre response

--- MobileApis/app/Http/Controllers/Api/RateController.php
+++ MobileApis/app/Http/Controllers/Api/RateController.php
@@ -10,8 +10,14 @@ class RateController extends Controller
 {
     public function getRate(){
         $url = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233121";
-        $lira = collect(Http::get($url)->json());
-
-        return $lira;
+        $lira = collect(Http::get($url)->json('buy'));
+        $rate = $lira->last()[1];
+        return response()->json([
+            'code' => 200,
+            'message' => 'Rate was retrieved successfully',
+            'data' => [
+                'rate' => $rate,
+            ],
+        ]);
     }
 }
