cc3ceda889dc8041325fed3603a5109e088e1a1b
husseinadkerning <hseneldowi@gmail.com>
2022-03-31 21:56:44 +0300

    add getRate in RateController

--- MobileApis/app/Http/Controllers/Api/RateController.php
+++ MobileApis/app/Http/Controllers/Api/RateController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class RateController extends Controller
 {
     public function getRate(){
+        $url = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233121";
+        $lira = Http::get($url)->json();
+
+        return $lira;
     }
 }
