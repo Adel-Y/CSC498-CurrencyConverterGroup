4ddb66fc898600ed22467d8ac3a31ae22692c88c
husseinadkerning <hseneldowi@gmail.com>
2022-03-31 22:04:03 +0300

    add try catch on guzzle call

--- MobileApis/app/Http/Controllers/Api/RateController.php
+++ MobileApis/app/Http/Controllers/Api/RateController.php
@@ -9,9 +9,19 @@ use Illuminate\Support\Facades\Http;
 class RateController extends Controller
 {
     public function getRate(){
-        $url = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233121";
-        $lira = collect(Http::get($url)->json('buy'));
-        $rate = $lira->last()[1];
+
+        try {
+            $url = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233121";
+            $lira = collect(Http::get($url)->json('buy'));
+            $rate = $lira->last()[1];
+        }catch (\Throwable $exception){
+            response()->json([
+                'code' => 200,
+                'message' => 'An error occurred',
+                'data' => null
+            ]);
+        }
+
         return response()->json([
             'code' => 200,
             'message' => 'Rate was retrieved successfully',
